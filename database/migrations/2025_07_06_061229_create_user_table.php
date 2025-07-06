689259aedb071521ea5ddafe0be3f71fb76a98ba
Ghani Mudzakir <ghanimdzkr3@gmail.com>
2025-07-06 14:18:22 +0800

    try to add some migration files

---
+++ database/migrations/2025_07_06_061229_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id_user');
+            $table->integer('id_ps');
+            $table->string('NIP');
+            $table->string('password');
+            $table->string('username');
+            $table->string('role');
+
+            // kita tambahi relasi foreign keynya
+            $table->foreign('id_ps')->references('id_ps')->on('program_studi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
