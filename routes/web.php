48c796e2977128b228189e5fcec3a756da3f4e85
BINZ <adrianbintang3@gmail.com>
2025-07-13 20:39:34 +0700

    Penambahan halaman login dan perbaikan halaman dasboard

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BerandaController;
 
 Route::get('/', [BerandaController::class, 'index']);
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
