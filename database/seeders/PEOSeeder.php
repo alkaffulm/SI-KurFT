fc31d8d5c1422f96175353f289a9913bc82c8771
Ghani Mudzakir <ghanimdzkr3@gmail.com>
2025-07-21 20:25:54 +0800

    new databaseseeder

--- database/seeders/PEOSeeder.php
+++ database/seeders/PEOSeeder.php
@@ -14,15 +14,15 @@ class PEOSeeder extends Seeder
     public function run(): void
     {
         DB::table('peo')->insert([
-            ['id_peo' => 1, 'kode_peo' => 'PEO-1', 'desc_peo' => 'Ini Deskripsi PEO-1'],
-            ['id_peo' => 2, 'kode_peo' => 'PEO-2', 'desc_peo' => 'Ini Deskripsi PEO-2'],
-            ['id_peo' => 3, 'kode_peo' => 'PEO-3', 'desc_peo' => 'Ini Deskripsi PEO-3'],
-            ['id_peo' => 4, 'kode_peo' => 'PEO-4', 'desc_peo' => 'Ini Deskripsi PEO-4'],
-            ['id_peo' => 5, 'kode_peo' => 'PEO-5', 'desc_peo' => 'Ini Deskripsi PEO-5'],
-            ['id_peo' => 6, 'kode_peo' => 'PEO-6', 'desc_peo' => 'Ini Deskripsi PEO-6'],
-            ['id_peo' => 7, 'kode_peo' => 'PEO-7', 'desc_peo' => 'Ini Deskripsi PEO-7'],
-            ['id_peo' => 8, 'kode_peo' => 'PEO-8', 'desc_peo' => 'Ini Deskripsi PEO-8'],
-            ['id_peo' => 9, 'kode_peo' => 'PEO-9', 'desc_peo' => 'Ini Deskripsi PEO-9'],
+            ['kode_peo' => 'PEO-1', 'desc_peo' => 'Ini Deskripsi PEO-1'],
+            ['kode_peo' => 'PEO-2', 'desc_peo' => 'Ini Deskripsi PEO-2'],
+            ['kode_peo' => 'PEO-3', 'desc_peo' => 'Ini Deskripsi PEO-3'],
+            ['kode_peo' => 'PEO-4', 'desc_peo' => 'Ini Deskripsi PEO-4'],
+            ['kode_peo' => 'PEO-5', 'desc_peo' => 'Ini Deskripsi PEO-5'],
+            ['kode_peo' => 'PEO-6', 'desc_peo' => 'Ini Deskripsi PEO-6'],
+            ['kode_peo' => 'PEO-7', 'desc_peo' => 'Ini Deskripsi PEO-7'],
+            ['kode_peo' => 'PEO-8', 'desc_peo' => 'Ini Deskripsi PEO-8'],
+            ['kode_peo' => 'PEO-9', 'desc_peo' => 'Ini Deskripsi PEO-9'],
 
         ]);
     }
