80ce31a4e504f2e927bf8db19ffb852686d4e790
AppleCraft2005 <jovangilbert737@gmail.com>
2026-03-06 12:22:14 +0800

    * fix user seeder password * add new prodi Rekayasa Teknik Komputer * change PEO description

--- database/seeders/ProgramStudiSeeder.php
+++ database/seeders/ProgramStudiSeeder.php
@@ -24,6 +24,8 @@ class ProgramStudiSeeder extends Seeder
             ['id_ps' => 7, 'nama_prodi' => 'Teknologi Informasi'],
             ['id_ps' => 8, 'nama_prodi' => 'Rekayasa Elektro'],
             ['id_ps' => 9, 'nama_prodi' => 'Rekayasa Geologi'],
+            ['id_ps' => 15, 'nama_prodi' => 'Teknik Rekayasa Teknik Komputer'],
+
             // Prodi S2
             ['id_ps' => 10, 'nama_prodi' => 'Magister Teknik Sipil'],
             ['id_ps' => 11, 'nama_prodi' => 'Magister Teknik Mesin'],
