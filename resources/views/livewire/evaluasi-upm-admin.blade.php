09f28c6f9d5e50e0fca1b9e911e729744fafb57a
AppleCraft2005 <jovangilbert737@gmail.com>
2026-04-30 20:31:49 +0800

    fix: evaluasi upm in admin layout

--- resources/views/livewire/evaluasi-upm-admin.blade.php
+++ resources/views/livewire/evaluasi-upm-admin.blade.php
@@ -21,7 +21,7 @@
         </div>
     @endif
 
-    <div class="py-8 px-16 ">
+    <div class="py-2 px-10 ">
         <main class="">
             <nav class="flex mb-4" aria-label="Breadcrumb">
                 <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
